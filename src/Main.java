b69d3508026497304778bfaa7c203965f105b483
void <v0id@riseup.net>
2024-03-19 16:43:38 -0300

    ejercicio1a6

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+
+public class Main {
+    public static void main(String[] args) {
+    /*region
+    //ejercicio1
+
+    int N = 10;
+    double a=12.5;
+    char c='A';
+     double add= N+a;
+     double sus= N-a;
+        System.out.print(add+"  "+sus+"  "+c);
+     /*endRegion*/
+       /*region
+     //ejercicio2
+      int x=10;
+      int y=12;
+      double n,m;
+      n=13.5;
+      m=10.75;
+        double res =n-m-x-y;
+        double div=m+n+x+y/n;
+
+        System.out.println("suma :"+ n+m+x+y  +" multiplicacion: " + n*m*x*y  );
+        System.out.println("resta "+res + " division : "+div );
+
+     /*endRegion*/
+      /*region
+      //ejercicio3
+
+        int N=60;
+        System.out.println("resultado :"+ ((N+77)-3)*2);
+
+      /*endRegion*/
+      /*region
+      //ejercicio4
+        int A=10;
+        int B=12;
+        int C=34;
+        int D=21;
+        B=C;
+        C=A;
+        A=D;
+        D=B;
+        System.out.println("a :"+A+" ,b :"+B+" ,c :"+C+" ,d :"+D );
+
+       /*endregion*/
+
+        //ejercico5
+       /*region
+        int a=14;
+        boolean par=false;
+        if(a %2 ==0){
+                      par=true;
+                    }
+        System.out.println("si es par true si es false no es par...: "+ par);
+         /*endRegion*/
+
+        //ejercicio6
+        int a=14;
+        if(a>0){
+            System.out.println("mayor a cero");
+               }else {
+            System.out.println("menor a cero");
+        }
+
+        }
+    }
