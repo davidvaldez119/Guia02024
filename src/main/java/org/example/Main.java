52fa250215cc5bedd8a94bcbeaff71ce114d9da4
LuchoDMD <lucio.mdq21@gmail.com>
2024-03-20 18:47:45 -0300

    ejercicio8

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //String nombre;
+        System.out.println("ingrese nombre :.....");
+        Scanner sl =new Scanner(System.in);
+        String name =sl.next();
+        System.out.println( name );
+
+
+
+
+    }
+}
