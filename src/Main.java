e735c16eeed60f8b211c81e3a3b06db5cc5c8312
void <v0id@riseup.net>
2024-03-21 19:39:28 -0300

    ejercicio16

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -57,12 +60,82 @@ public class Main {
          /*endRegion*/
 
         //ejercicio6
-        int a=14;
+        /*int a=14;
         if(a>0){
             System.out.println("mayor a cero");
                }else {
             System.out.println("menor a cero");
         }
-
+*/
         }
+         /*
+        //ejercico8 guia 0
+        System.out.println("ingrese nombre :.....");
+        Scanner sl =new Scanner(System.in);
+        String name =sl.next();
+        System.out.println( name );
+
+        */
+        /*
+         //ejercicio9
+
+        System.out.println("ingrese un numero:.....");
+        Scanner num =new Scanner(System.in);
+        Integer nume=num.nextInt();
+        Integer doble=2*nume;
+        Integer triple=3*nume;
+        System.out.println("el doble es:.... "+doble +"   "+"el triple es:... "+triple);
+          */
+
+    //ejercicio10
+        /*
+        System.out.println("ingrese los grados farenheith:.....");
+        Scanner num =new Scanner(System.in);
+        double nume=num.nextInt();
+        double f=( (nume-32)*5)/9;
+        System.out.println("grados celcius :   "+f );
+         */
+    //ejercicio11
+       /*
+        System.out.println("ingrese el radio de circunferencia:.....");
+        Scanner num =new Scanner(System.in);
+        double radio=num.nextInt();
+        double p=2*(3.1416)*radio;
+        double a=(3.1416)*radio*radio;
+        System.out.println("perimetro circunferencia :..."+p+" area: ...."+a );
+         */
+    //regionejercicio17
+
+       /*
+
+
+        System.out.println("ingrese el numero:.....");
+        Scanner scaner = new Scanner(System.in);
+        Integer num = scaner.nextInt();
+        String str = num.toString();
+
+
+        for (Integer i = 0; i < 5; i++) {
+            if (i % 2 == 0) {
+                System.out.println(str.charAt(i));
+            }
+        }
+
+        //endregion
+        */
+    //regionejercicio16
+    //ejercicio16
+
+
+     System.out.println("ingrese el numero:.....");
+
+    Scanner scanner=new Scanner(System.in);
+    Integer num = scanner.nextInt();
+    String str=num.toString();
+
+    for (Integer i=0; i<2 ; i++){
+
+        System.out.println(str.charAt(i));
+    }
+//endregion
     }
